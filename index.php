c70f18ce451432825a2337ef02517112aeaecf75
imofficialbunty <79312744+imofficialbunty@users.noreply.github.com>
2021-02-19 20:26:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>The spark foundation</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Potta+One&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+</head>
+<body>
+   
+   <div class="banner-area">
+      <header>
+      <a href="#" class="logo">Bunty</a>
+      <ul class="menu">
+    	<li><a href="#">Home</a></li>
+    	<li><a href="#">Contact</a></li>
+    </ul> 
+    </header>	
+
+   <div class="banner-text">
+   	 <h2>The Spark Foundation Task</h2>
+   	 <p> It Is completed by Bunty</p>
+   	  <a href="PayUMoney_form.php">Donate</a>
+   </div>
+   </div>
+
+</body>
+</html>
